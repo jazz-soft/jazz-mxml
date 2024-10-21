7f641a264a6a79df212869eae974d04bec43ccd1
Sema <jazz-dev@jazz-soft.net>
2024-10-21 19:14:47 -0400

    Time/Part2Time()

--- jazz-mxml.js
+++ jazz-mxml.js
@@ -9,10 +9,26 @@ function MXML(s) {
   if (!xml['score-partwise'] && !xml['score-timewise']) return;
   this.txt = s;
   this.xml = parser.parse(s);
-//console.log('XML:', this.xml);
 }
 MXML.prototype.isValid = function() { return !!this.xml; };
 MXML.prototype.isPartwise = function() { return this.isValid() && !!this.xml['score-partwise']; };
 MXML.prototype.isTimewise = function() { return this.isValid() && !!this.xml['score-timewise']; };
+MXML.prototype.Part2Time = function() {};
+MXML.prototype.Time2Part = MXML.prototype.Part2Time;
+
+if (typeof DOMParser != 'undefined') {
+  const dom_parser = new DOMParser();
+  const xml_serial = new XMLSerializer();
+  const xsl_p2t = new XSLTProcessor();
+  const xsl_t2p = new XSLTProcessor();
+  xsl_p2t.importStylesheet(parser.parseFromString(specs.p2t, "text/xml"));
+  xsl_t2p.importStylesheet(parser.parseFromString(specs.t2p, "text/xml"));
+  MXML.prototype.Part2Time = function() {
+    if (this.isPartwise()) return xml_serial.serializeToString(xsl_p2t.transformToDocument(dom_parser.parseFromString(this.getText(), "text/xml")));
+  };
+  MXML.prototype.Time2Part = function() {
+    if (this.isPartwise()) return xml_serial.serializeToString(xsl_t2p.transformToDocument(dom_parser.parseFromString(this.getText(), "text/xml")));
+  };
+}
 
 module.exports = MXML;
