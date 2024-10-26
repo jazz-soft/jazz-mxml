40b48496e75331033dd9865144f95f6762edc88b
Sema <jazz-dev@jazz-soft.net>
2024-10-26 11:47:12 -0400

    Typo fix

--- specs/specs.js
+++ specs/specs.js
@@ -3,7 +3,7 @@ const {XMLParser, XMLBuilder} = require('fast-xml-parser');
 
 var i;
 var body = [];
-body.push("const mxlm_xsd = '" + read_xml('musicxml.xsd') + "';");
+body.push("const mxml_xsd = '" + read_xml('musicxml.xsd') + "';");
 body.push("const p2t_xsl = '" + read_xml('parttime.xsl') + "';");
 body.push("const t2p_xsl = '" + read_xml('timepart.xsl') + "';");
 
